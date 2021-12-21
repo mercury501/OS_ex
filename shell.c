a89ab873ed93e2f804b4fa22e46727c62606a2a6
mercury501 <tommaso.desoldaee@gmail.com>
2021-12-21 13:30:08 +0100

    changed shell implementation

--- shell.c
+++ shell.c
@@ -3,36 +3,45 @@
 #include <stdio.h>
 #include <string.h>
 
-char ** tokenize_str(char *str){    //divido il comando inserito in comandi, separati da uno spazio
+int tokenize_str(char *str, char **command) {    //divido il comando inserito in comandi, separati da uno spazio
     int i = 0;
     char *temp;
-    char ** command = malloc(sizeof(char *) * 6);   //alloco spazio per 6 puntatori stringa
+
     
     temp = strtok(str, " ");
 
-    while (temp != NULL){
+    while (temp != NULL) {
         command[i] = malloc(sizeof(char) * strlen(temp));   
         strcpy(command[i], temp);   //salvo la stringa corrente nell'array di comandi
         temp = strtok(NULL, " ");
         i++;
     }
     command[i - 1][strlen(command[i - 1]) - 1] = 0;
-    return command;
+    return i;
 }
 
-int main(void){
+
+
+int main(void) {
     int pid, status;
     char buffer[128];
-
+    char ** command = malloc(sizeof(char *) * 128);   //alloco spazio per 128 puntatori stringa
+    int commandNum;
     
     printf("%%");
 
-    while (fgets(buffer, 128, stdin)){
-        char **command = tokenize_str(buffer);
+    while (fgets(buffer, 128, stdin)) {
+        commandNum = tokenize_str(buffer, command);
+        command[commandNum] = NULL;
 
-
+        
         if (strstr(command[0], "exit") != NULL) //se il comando comincia con exit, termina la shell
             return 0;
+        
+        if (strstr(command[0], "shinfo") != NULL) {
+            printf("mercury shell!\n");
+            break;
+        }
         
         if ((pid = fork()) < 0){
             printf("ERRORE: fork\n");
@@ -41,7 +50,7 @@ int main(void){
 
         else 
             if (pid == 0){ //processo figlio
-                if (execlp(command[0], command[0], command[1], command[2], command[3], command[4], command[5], NULL) < 0) {
+                if (execvp(command[0], command) < 0) {  //execvp prende come primo argomento il comando, come secondo un array con comando + argomenti + NULL
                     printf("ERRORE: execute\n");
                     return 1;
                 }
@@ -49,7 +58,7 @@ int main(void){
             return 0;
         }
 
-        if (pid == waitpid(pid, &status, 0) < 0){
+        if (pid == waitpid(pid, &status, 0) < 0) {
             printf("ERRORE: waitpid\n");
             return 1;
         }
